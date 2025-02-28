5d7a15337a0b89a1ff17a18aa6207e44e12d91a1
Farzin Shams <farzinnater@gmail.com>
2025-02-28 18:03:49 +0330

    Change Base Configuration

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class StatusType(models.IntegerChoices):
+    todo = 1, _("To Do")
+    doing = 2, _("Doing")
+    done = 3, _("Done")
+
+
+class ToDo(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    status = models.IntegerField(
+        choices=StatusType.choices, default=StatusType.todo.value)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_date']
+
+    def __str__(self):
+        return self.title
