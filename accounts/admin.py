a31d86032ce8a97415ccdc5435c4567d2bff9ba8
Farzin Shams <farzinnater@gmail.com>
2025-02-28 17:18:36 +0330

    Base Configuration of Core

---
+++ accounts/admin.py
@@ -0,0 +1,38 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Profile
+from .forms import UserCreationForm, UserChangeForm
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+    list_display = ("email", "is_superuser", "is_staff", "is_active",)
+    list_filter = ("email", "is_staff", "is_active",)
+    fieldsets = (
+        ('Authentication', {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_superuser", "is_staff", "is_active")}),
+        ("Group Permissions", {"fields": ("groups", "user_permissions")}),
+        ("User Logs", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        ('Authentication', {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password1", "password2", "is_superuser", "is_staff",
+                "is_active"
+            )}
+         ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user", "first_name", "last_name", "phone_number")
+    searching_fields = ("user", "first_name", "last_name", "phone_number")
+
+
+admin.site.register(User, CustomUserAdmin)
---
+++ accounts/models.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from .managers import UserManager
+from .validators import iranian_phone_number_validator
+
+
+# Create your models here.
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    Custom User Model by AbstractBaseUser
+    """
+    email = models.EmailField(_("email address"), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    # is_verified = models.BooleanField(default=False)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        'User', on_delete=models.CASCADE, related_name="user_profile")
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=15, validators=[
+                                    iranian_phone_number_validator])
+    avatar = models.ImageField(
+        upload_to="accounts/profile/", default="profile/default.jpg")
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.email
+
+    def get_fullname(self):
+        if self.first_name or self.last_name:
+            return f"{self.first_name} {self.last_name}"
+        else:
+            return "پروفایل خود را تکمیل کنید"
