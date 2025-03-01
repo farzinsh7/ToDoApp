9e1a0d13762ff5eed62e6985b8c043ee9596763b
Farzin Shams <farzinnater@gmail.com>
2025-03-01 15:55:54 +0330

    Finish Update View

--- todo/urls.py
+++ todo/urls.py
@@ -5,4 +5,8 @@ from . import views
 app_name = "todo"
 urlpatterns = [
     path('', views.ToDoView.as_view(), name='index'),
+    path("todo/<int:pk>/update/",
+         views.ToDoUpdateview.as_view(), name="todo-update"),
+    path("todo/<int:pk>/delete/",
+         views.ToDoDeleteView.as_view(), name="todo-delete"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
@@ -17,3 +17,20 @@ class ToDoView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = ToDo.objects.all()
         return context
+    
+    
+
+class ToDoDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    queryset = ToDo.objects.all()
+    http_method_names = ["post"]
+    success_message = "The skill was Deleted successfully."
+    success_url = reverse_lazy("todo:index")
+
+
+
+class ToDoUpdateview(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    template_name = "todo/todo-update.html"
+    queryset = ToDo.objects.all()
+    form_class = ToDoFormClass
+    success_message = "The Task was Updated successfully."
+    success_url = reverse_lazy("todo:index")
