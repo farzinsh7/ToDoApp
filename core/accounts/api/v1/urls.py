2e75891ca910e01530dffb35da25abbf8799ab98
Farzin Shams <farzinnater@gmail.com>
2025-04-04 13:51:38 +0330

    added logout for discarding token

--- core/accounts/api/v1/urls.py
+++ core/accounts/api/v1/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     # Authontication by auth-token
     path("token/login/", views.CustomObtainAuthToken.as_view(), name="toke-login"),
+    path("token/logout/", views.CustomDiscardAuthToken.as_view(), name="token-logout"),
 
 ]
--- core/accounts/api/v1/views.py
+++ core/accounts/api/v1/views.py
@@ -2,6 +2,9 @@ from rest_framework.authtoken.views import ObtainAuthToken
 from .serializers import CustomAuthTokenSerializer
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework import status
 
 
 class CustomObtainAuthToken(ObtainAuthToken):
@@ -15,3 +18,11 @@ class CustomObtainAuthToken(ObtainAuthToken):
         user = serializer.validated_data["user"]
         token, created = Token.objects.get_or_create(user=user)
         return Response({"token": token.key, "user_id": user.pk, "email": user.email})
+
+
+class CustomDiscardAuthToken(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
